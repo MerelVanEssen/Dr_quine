19f08cde6faa12b2880e1cf57c8359f30f358371
Merel <mvan-ess@codam.student.nl>
2026-01-29 11:33:21 +0100

    sully

--- ASM/Grace.s
+++ ASM/Grace.s
@@ -1,20 +1,41 @@
 global main
-extern printf
+extern fopen
+extern fprintf
+extern fclose
+
+%macro OPEN_FILE 2
+lea rdi, [rel %1]
+lea rsi, [rel %2]
+call fopen wrt ..plt
+mov rbx, rax
+%endmacro
+
+%macro WRITE_FILE 3
+mov rdi, rbx
+lea rsi, [rel %1]
+mov edx, %2
+mov ecx, %3
+lea r8,  [rel %1]
+xor eax, eax
+call fprintf wrt ..plt
+%endmacro
+
+%macro RUN 0
+; one comment
+sub rsp, 8
+OPEN_FILE filename, mode
+WRITE_FILE text, 10, 34
+mov rdi, rbx
+call fclose wrt ..plt
+add rsp, 8
+ret
+%endmacro
 
 section .text
 main:
-; one comment
-push rbp
-mov rbp, rsp
-lea rdi, [rel text]
-mov rsi, 10
-mov rdx, 34
-mov r8, 37
-lea rcx, [rel text]
-xor eax, eax
-call printf wrt ..plt
-leave
-ret
+RUN
 
 section .data
-text db "global main%1$cextern printf%1$c%1$csection .text%1$cMAIN%1$c; one comment%1$cpush rbp%1$cmove rbp, rsp%1$clea rdi, [rel text]%1$cmove rsi, 10%1$cmov rdx,34%1$cmov r8, 37%1$clea rcx, [rel text]%1$cxor eax, eax%1$ccall printf wrt ..plt%1$cleave%1$cret%1$c%1$csection .data%1$ctext db %2$c%3$s%2$c"
+filename db "Grace_kid.s",0
+mode db "w",0
+text db "global main%1$cextern fopen%1$cextern fprintf%1$cextern fclose%1$c%1$c%%macro OPEN_FILE 2%1$clea rdi, [rel %%1]%1$clea rsi, [rel %%2]%1$ccall fopen wrt ..plt%1$cmov rbx, rax%1$c%%endmacro%1$c%1$c%%macro WRITE_FILE 3%1$cmov rdi, rbx%1$clea rsi, [rel %%1]%1$cmov edx, %%2%1$cmov ecx, %%3%1$clea r8,  [rel %%1]%1$cxor eax, eax%1$ccall fprintf wrt ..plt%1$c%%endmacro%1$c%1$c%%macro RUN 0%1$c; one comment%1$csub rsp, 8%1$cOPEN_FILE filename, mode%1$cWRITE_FILE text, 10, 34%1$cmov rdi, rbx%1$ccall fclose wrt ..plt%1$cadd rsp, 8%1$cret%1$c%%endmacro%1$c%1$csection .text%1$cmain:%1$cRUN%1$c%1$csection .data%1$cfilename db %2$cGrace_kid.s%2$c,0%1$cmode db %2$cw%2$c,0%1$ctext db %2$c%3$s%2$c"
---
+++ ASM/Sully.s
@@ -0,0 +1,79 @@
+global main
+extern system
+extern fopen
+extern fprintf
+extern fclose
+
+%macro OPEN_FILE 2
+lea rdi, [rel %1]
+lea rsi, [rel %2]
+call fopen wrt ..plt
+mov rbx, rax
+%endmacro
+
+%macro WRITE_FILE 4
+mov rdi, rbx
+lea rsi, [rel %1]
+mov edx, %2
+mov ecx, %3
+;mov r8, %4
+;lea r8, [rel %1]
+xor eax, eax
+call fprintf wrt ..plt
+%endmacro
+
+%macro FILL 2
+lea rbx, [rel %1]
+lea rcx, [rel %2]
+mov dl, [rel start]
+add dl, 48
+mov rsi, rcx
+mov rdi, rbx
+%%copy_loop:
+mov al, [rcx]
+cmp al, 0
+je %%done
+cmp al, 'X'
+jne %%copy_normal
+mov al, dl
+%%copy_normal:
+mov [rbx], al
+inc rcx
+inc rbx
+jmp %%copy_loop
+%%done:
+nop
+%endmacro
+
+section .text
+main:
+FILL compile_bss, compile_data
+FILL run_bss, run_data
+FILL filename_bss, filename_data
+sub rsp, 8
+OPEN_FILE filename_bss, mode
+mov dl, [rel start]
+add dl, 48
+movzx r8, dl
+WRITE_FILE text, 10, 34, dl
+mov rdi, rbx
+call fclose wrt ..plt
+lea rdi, [rel compile_bss]
+call system wrt ..plt
+lea rdi, [rel run_bss]
+call system wrt ..plt
+add rsp, 8
+ret
+
+section .bss
+filename_bss resb 10
+compile_bss resb 32
+run_bss resb 16
+
+section .data
+start db 5
+mode db "w",0
+filename_data db "Sully_X.s",0
+compile_data db "gcc Sully_X.s -o Sully_X",0
+run_data db "./Sully_X",0
+text db "global main%1$cextern system%1$cextern fopen%1$cextern fprintf%1$cextern fclose%1$c%1$c%%macro OPEN_FILE 2%1$clea rdi, [rel %%1]%1$clea rsi, [rel %%2]%1$ccall fopen wrt ..plt%1$cmov rbx, rax%1$c%%endmacro%1$c%1$c%%macro WRITE_FILE 3%1$cmov rdi, rbx%1$clea rsi, [rel %%1]%1$cmov edx, %%2%1$cmov ecx, %%3%1$clea r8,  [rel %%1]%1$cxor eax, eax%1$ccall fprintf wrt ..plt%1$c%%endmacro%1$c%1$c%macro FILL 2%1$clea rbx, [rel %%1]%1$clea rcx, [rel %%2]%1$cmov dl, [rel start]%1$cadd dl, 48%1$cmov rsi, rcx%1$cmov rdi, rbx%1$c%%%%copy_loop:%1$cmov al, [rcx]%1$ccmp al, 0%1$cje %%done%1$ccmp al, 'X'%1$cjne %%%%copy_normal%1$cmov al, dl%1$c%%%%copy_normal:%1$cmov [rbx], al%1$cinc rcx%1$cinc rbx%1$cjmp %%copy_loop%1$c%%%%done:%1$cnop%1$c%%endmacro%1$c%1$csection .text%1$cmain:%1$cFILL compile_bss, compile_data%1$cFILL run_bss, run_data%1$cFILL filename_bss, filename_data%1$csub rsp, 8%1$cOPEN_FILE filename_bss, mode%1$cWRITE_FILE text, 10, 34%1$cmov rdi, rbx%1$ccall fclose wrt ..plt%1$clea rdi, [rel compile_bss]%1$ccall system wrt ..plt%1$clea rdi, [rel run_bss]%1$ccall system wrt ..plt%1$cadd rsp, 8%1$cret%1$c%1$csection .bss%1$cfilename_bss resb 10%1$ccompile_bss resb 32%1$crun_bss resb 16%1$c%1$csection .data%1$cstart db %4$c%1$cmode db %2$cw%2$c,0%1$cfilename_data db %2$cSully_X.s%2$c,0%1$ccompile_data db %2$cgcc Sully_X.s -o Sully_X%2$c,0%1$crun_data db %2$c./Sully_X%2$c,0%1$ctext db %2$c%3$c%2$c,0",0
